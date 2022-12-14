f8a00884cc3638b1fbc99981b39d83cd573b9574
a2110l6009 <a2110l6009@grad.ntue.edu.tw>
2022-12-14 19:09:02 +0800

    2022.12.14work

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("hello hello NTUE!")
+#print("hello hello NTUE!")
+
+import random
+
+low_bound = 1
+up_bound = 100
+
+answer = random.randint(low_bound, up_bound)
+
+count = 5
+while count:
+    
+    guess = int(input(f"Type your guess({low_bound}~{up_bound}): "))
+
+    if guess < answer:
+        low_bound = guess
+    elif guess > answer:
+        up_bound = guess
+    else:
+        print("You're corrects!")
+        break
+    
+    count -= 1
